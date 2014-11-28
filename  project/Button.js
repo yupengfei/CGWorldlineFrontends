95ecc59291de55856809b77e2a289a2896ea8283
whyhaiyan <1562822728@qq.com>
2014-11-28 13:46:26 +0800

    前面传的都不要了！

---
+++  project/Button.js
@@ -0,0 +1,27 @@
+function  Submit()
+{
+   
+   var a=  window.document.getElementById();
+   var b＝
+
+}
+function txtChang()
+    {         
+            var td_otheraddr=document.getElementById('textButton1');
+            var td_address=document.getElementById('td_address');
+            var content=document .getElementById("content");
+            if(this.txtRoad.Value == "其它(区外)")
+          {        
+            td_otheraddr.style.display="block";
+            td_address.style.display="none";
+             content.style.display="none";
+         }
+          else
+          {
+           td_otheraddr.style.display="none";
+            td_address.style.display="block";
+            content.style.display="block";
+
+          }
+        
+    }
